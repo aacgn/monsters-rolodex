413a76b16593bc65ab645b397995e648d17c3312
aacgn <4augusto@gmail.com>
2019-12-29 01:16:09 -0300

    Updates

---
+++ precache-manifest.d5f861c0c26996a340e64aa391c86c71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11716757d0e076f32d51bc2c404c9450",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "0f07d62db9fc36764ba9",
+    "url": "/monsters-rolodex/static/css/main.af163380.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0f07d62db9fc36764ba9",
+    "url": "/monsters-rolodex/static/js/main.75f25431.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
